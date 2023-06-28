f597d49d15e8cff15d219b9cd745c4d8e201f05c
Kuznetcov Igor <kuznetcov28@mail.ru>
2023-06-28 13:56:10 +0300

    Initial Commit

---
+++ View and three sliders/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  View and three sliders
+//
+//  Created by Игорь Кузнецов on 6/28/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
