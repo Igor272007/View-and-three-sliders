4315618465572c3550c5ce64082fcd2db818455b
Kuznetcov Igor <kuznetcov28@mail.ru>
2023-06-29 12:37:33 +0300

    переделал все в очередной раз и забыл про коммиты. уже нещадно подглядывал в поисках ошибок

--- View and three sliders/ViewController.swift
+++ View and three sliders/ViewController.swift
@@ -8,9 +8,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var viewColor: UIView!
     
+    
+    @IBOutlet weak var redLabel: UILabel!
+    @IBOutlet weak var greenLabel: UILabel!
+    @IBOutlet weak var blueLabel: UILabel!
     
     @IBOutlet weak var redSlider: UISlider!
     @IBOutlet weak var greenSlider: UISlider!
@@ -18,32 +22,53 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-       
+        
         //View
         viewColor.layer.cornerRadius = 10
         
         //Sliders
+        redSlider.minimumTrackTintColor = .red
+        redSlider.value = 0
+        redSlider.minimumValue = 0
+        redSlider.maximumValue = 1
+        redSlider.thumbTintColor = .red
+        
+        greenSlider.minimumTrackTintColor = .green
+        greenSlider.value = 0
+        greenSlider.minimumValue = 0
+        greenSlider.maximumValue = 1
+        greenSlider.thumbTintColor = .green
+        
+        blueSlider.value = 0
+        blueSlider.minimumValue = 0
+        blueSlider.maximumValue = 1
+        blueSlider.thumbTintColor = .blue
+        
         viewColor.backgroundColor = UIColor(
             red: CGFloat(redSlider.value),
             green: CGFloat(greenSlider.value),
             blue: CGFloat(blueSlider.value),
             alpha: 1
         )
-
-//        //TextFields
-//        redTextField.text = String(format: "%.2f", redSlider.value)
-//        greenTextField.text = String(format: "%.2f", greenSlider.value)
-//        blueTextField.text = String(format: "%.2f", blueSlider.value)
+        
+        //Red Green Blue Labels
+        redLabel.text = String(format: "%.2f", redSlider.value)
+        greenLabel.text = String(format: "%.2f", greenSlider.value)
+        blueLabel.text = String(format: "%.2f", blueSlider.value)
         
     }
-
+    
     @IBAction func allSlidersAction(_ sender: UISlider) {
         viewColor.backgroundColor = UIColor(
             red: CGFloat(redSlider.value),
             green: CGFloat(greenSlider.value),
             blue: CGFloat(blueSlider.value),
             alpha: 1
-            )
-    }
+        )
         
+        redLabel.text = String(format: "%.2f", redSlider.value)
+        greenLabel.text = String(format: "%.2f", greenSlider.value)
+        blueLabel.text = String(format: "%.2f", blueSlider.value)
+    }
+    
 }
