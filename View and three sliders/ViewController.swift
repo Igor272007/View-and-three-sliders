c7581b0c03e46fb62ab992749422359fc7c902f6
Kuznetcov Igor <kuznetcov28@mail.ru>
2023-06-29 11:24:11 +0300

    changes textFields to views

--- View and three sliders/ViewController.swift
+++ View and three sliders/ViewController.swift
@@ -9,25 +9,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var viewColor: UIView!
+    
+    
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+       
         //View
-    //    viewColor.layer.cornerRadius = 10
+        viewColor.layer.cornerRadius = 10
         
-        
+        //Sliders
+        viewColor.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1
+        )
+
+//        //TextFields
+//        redTextField.text = String(format: "%.2f", redSlider.value)
+//        greenTextField.text = String(format: "%.2f", greenSlider.value)
+//        blueTextField.text = String(format: "%.2f", blueSlider.value)
         
     }
 
-//    {
-//        viewColor.backgroundColor = viewColor.backgroundColor?.withAlphaComponent(CGFloat(redSlider.value))
-//    }
-//
-//    {
-//        viewColor.backgroundColor = viewColor.backgroundColor?.withAlphaComponent(CGFloat(greenSlider.value))
-//    }
-//
-//   {
-//    }
+    @IBAction func allSlidersAction(_ sender: UISlider) {
+        viewColor.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1
+            )
+    }
+        
 }
-
